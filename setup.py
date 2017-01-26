28bc508b5e6bac0afac503688cb09ce5acb9eeb8
woutercoppens <wouter.coppens@gmail.com>
2017-01-26 21:58:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='openmotics-sdk',
       description='OpenMotics Python SDK for communication with the OpenMotics Gateway.',
       author='OpenMotics',
       author_email='info@OpenMotics.com',
-      url='https://github.com/woutercoppens/openmotics-sdk',
-      packages=['openmotics-sdk']
+      url='https://github.com/woutercoppens/openmoticssdk',
+      packages=['openmoticssdk']
      )
