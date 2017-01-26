6747527ea56f5e7d5e92c3c779873517e1a2f78c
woutercoppens <wouter.coppens@gmail.com>
2017-01-26 21:10:32 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='openmotics-sdk',
+      version='0.0.1',
+      description='OpenMotics Python SDK for communication with the OpenMotics Gateway.',
+      author='OpenMotics',
+      author_email='info@OpenMotics.com',
+      url='https://github.com/woutercoppens/openmotics-sdk',
+      packages=['openmotics-sdk']
+     )
