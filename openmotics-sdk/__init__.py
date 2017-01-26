8098e95033d8c48d1ca802fc45b622b377650445
woutercoppens <wouter.coppens@gmail.com>
2017-01-26 21:06:24 +0100

    Create __init__.py

---
+++ openmotics-sdk/__init__.py
@@ -0,0 +1,9 @@
+"""
+SDK for OpenMotics 
+Open source home automation platform.
+http://www.openmotics.com/
+:copyright: (c) 2016 by OpenMotics.
+:license: MIT, see LICENSE for more details.
+"""
+
+from sdk import OpenMoticsApi, OpenMoticsCloudApi, traceback
