bb03366ec8846bff2f47b97527f98ef5c0ef1c23
woutercoppens <wouter.coppens@gmail.com>
2017-01-26 22:19:08 +0100

    Update __init__.py

--- openmoticssdk/__init__.py
+++ openmoticssdk/__init__.py
@@ -5,5 +5,6 @@ http://www.openmotics.com/
 :copyright: (c) 2016 by OpenMotics.
 :license: MIT, see LICENSE for more details.
 """
+__version__ = '0.0.1'
 
 from .sdk import OpenMoticsApi, OpenMoticsCloudApi, traceback
